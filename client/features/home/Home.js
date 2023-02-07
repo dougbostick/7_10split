9c4cc745c223ad2169ebe0de409d64ba5432ba7e
Doug Bostick <doug.bostick@fullstackacademy.com>
2023-02-07 12:26:01 -0500

    added featured ball/shoe for home

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchSingleProduct,
+  selectSingleProduct,
+} from "../products/singleProductSlice";
 
-/**
- * COMPONENT
- */
-const Home = (props) => {
+const Home = () => {
   const username = useSelector((state) => state.auth.me.username);
+  const dispatch = useDispatch();
+  const [featuredBall, setFeaturedBall] = useState({});
+  const [featuredShoe, setFeaturedShoe] = useState({});
+
+  console.log(featuredBall);
+  console.log(featuredShoe);
+
+
+
+  const randomBallNum = Math.ceil(Math.random() * 15);
+  console.log(randomBallNum);
+  const randomShoeNum = Math.ceil(Math.random() * 24 + 16);
+  console.log(randomShoeNum);
+
+  useEffect(() => {
+    dispatch(fetchSingleProduct(randomBallNum)).then((res) => setFeaturedBall(res.payload));
+    dispatch(fetchSingleProduct(randomShoeNum)).then((res) => setFeaturedShoe(res.payload));
+  }, [dispatch])
 
   return (
     <div>
       <div>
-      <h3>Welcome, {username}</h3>
+        <h3>Welcome, {username}</h3>
       </div>
       <div>
-        <hr/>
-        Ball of the week
+        <h3>Featured Ball</h3>
+        {featuredBall.name}
       </div>
       <div>
-      <hr/>
-        Shoe of the week
+        <h3>Featured Shoes</h3>
+        {featuredShoe.name}
       </div>
     </div>
   );
