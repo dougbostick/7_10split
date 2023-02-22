3b6cbd874a948d7d113335fbc9607bdece54ccd4
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-02-21 19:45:40 -0500

    trying heroku pg url

--- server/db/db.js
+++ server/db/db.js
@@ -22,8 +22,9 @@ const config = {
 // }
 
 const db = new Sequelize(
-  'postgres://0.0.0.0:5432/7_10splitdb' ||
-    `postgres://localhost:5432/7_10splitdb`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/7_10splitdb`,
   config
 );
 module.exports = db;
+
+//postgres://hitkekkxwzqhsa:ac8817e2c8dbeb707759d057a5839b371a133df3ac2d10a181d3c586f2887217@ec2-34-192-210-139.compute-1.amazonaws.com:5432/d1krrtamjg5pco
