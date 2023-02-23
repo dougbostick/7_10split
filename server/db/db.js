121a00c7e2dd90025145655e6c8d4b3c019a1a96
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-02-23 11:57:42 -0500

    heroku ticket

--- server/db/db.js
+++ server/db/db.js
@@ -21,16 +21,16 @@ const client = new Client({
 
 client.connect();
 
-// client.query(
-//   'SELECT table_schema,table_name FROM information_schema.tables;',
-//   (err, res) => {
-//     if (err) throw err;
-//     for (let row of res.rows) {
-//       console.log(JSON.stringify(row));
-//     }
-//     client.end();
-//   }
-// );
+client.query(
+  'SELECT table_schema,table_name FROM information_schema.tables;',
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 // const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
