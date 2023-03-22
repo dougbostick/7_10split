80e397da5f7ee004ead4ae6f496ebe410d3c467f
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-03-22 16:28:56 -0400

    heroku tried to get me

--- server/db/db.js
+++ server/db/db.js
@@ -7,10 +7,10 @@ const config = {
 };
 
 const db = new Sequelize({
-  database: 'd1krrtamjg5pco',
-  username: 'hitkekkxwzqhsa',
-  password: 'ac8817e2c8dbeb707759d057a5839b371a133df3ac2d10a181d3c586f2887217',
-  host: 'ec2-34-192-210-139.compute-1.amazonaws.com',
+  database: 'd7dpuqjp8c082s',
+  username: 'kgiupzwyezgrel',
+  password: '2a2fa9ee4dadbd5e4fdd4c92d42b8ebd8b5aafd6496e19877a60656627777052',
+  host: 'ec2-50-17-21-26.compute-1.amazonaws.com',
   port: 5432,
   dialect: 'postgres',
   dialectOptions: {
