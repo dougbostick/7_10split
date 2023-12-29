8e0c45d14023508d72653d8796717361814ca707
Doug Bostick <dougbostick923@gmail.com>
2023-12-29 15:21:24 -0500

    working on carousel

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import CarouselComp from './CarouselComp';
+
+import Carousel from 'react-multi-carousel';
+
 import { useSelector, useDispatch } from 'react-redux';
 import {
   fetchSingleProduct,
@@ -25,6 +27,24 @@ const Home = () => {
     );
   }, [dispatch]);
 
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
   return (
     <div className="homePage">
       <div className="welcome">Welcome, {username}</div>
@@ -36,8 +56,26 @@ const Home = () => {
           welcome! Let's bowl!" -Sole Engineer, Doug Bostick
         </p>
       </div>
-
-      <CarouselComp />
+      <Carousel
+        responsive={responsive}
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={['tablet', 'mobile']}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Carousel>
       <div className="featureDiv">
         <div className="featureInfo">
           <h3>Featured Ball</h3>
