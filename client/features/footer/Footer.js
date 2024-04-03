f94dc3de4c0cc5782f0486bf763ba6518100f640
Doug Bostick <dougbostick923@gmail.com>
2024-04-02 20:25:09 -0400

    changed footer styles

--- client/features/footer/Footer.js
+++ client/features/footer/Footer.js
@@ -4,18 +4,61 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="footerText">
-        <p>Engineer: Doug Bostick</p>
-        <p>Follow links to learn more!</p>
+        <p>About this site:</p>
+        <p>
+          This site was built by Full Stack Software Engineer,
+          <br />
+          Doug Bostick.
+        </p>
+        <p>Check out these links to learn more!</p>
       </div>
-      <div className="footerImages">
-        <a href="https://www.linkedin.com/in/doug-bostick/" target="_blank">
-          <img src="/images/linkedin.png" className="footerImg" />
+
+      <div className="empty"></div>
+      <div className="footerLinks">
+        <a
+          className="footerLink"
+          href="https://dougbosticktech.com"
+          target="_blank"
+        >
+          Portfolio
+          {/* <img src="/images/halfwayCircle.png" className="footerImg" /> */}
         </a>
-        <a href="https://github.com/dougbostick" target="_blank">
-          <img src="/images/github.png" className="footerImg" />
+        <a
+          className="footerLink"
+          href="https://www.linkedin.com/in/doug-bostick/"
+          target="_blank"
+        >
+          Linkedin
+          {/* <img src="/images/linkedin.png" className="footerImg" /> */}
         </a>
-        <a href="https://dougbostick.github.io/react_halfway/" target="_blank">
-          <img src="/images/halfwayCircle.png" className="footerImg" />
+        <a
+          className="footerLink"
+          href="https://github.com/dougbostick"
+          target="_blank"
+        >
+          Github
+          {/* <img src="/images/github.png" className="footerImg" /> */}
+        </a>
+        <a className="footerLink" href="https://usemise.com" target="_blank">
+          Mise
+          {/* <img src="/images/halfwayCircle.png" className="footerImg" /> */}
+        </a>
+        <a
+          className="footerLink"
+          href="https://dougbostick.github.io/react_halfway/"
+          target="_blank"
+        >
+          Halfway
+          {/* <img src="/images/halfwayCircle.png" className="footerImg" /> */}
+        </a>
+
+        <a
+          className="footerLink"
+          href="https://word-game-nu.vercel.app/"
+          target="_blank"
+        >
+          Word Rush
+          {/* <img src="/images/halfwayCircle.png" className="footerImg" /> */}
         </a>
       </div>
     </div>
